7102093854bb4da0930db8f2cb499fd009b10a97
jagadeesh-sagar <nagarajug176@gmail.com>
2024-06-09 17:14:27 +0530

    Add webcam video setup

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import time
+
+video=cv2.VideoCapture(0)
+time.sleep(2)
+
+while True:
+    check, frame=video.read()
+    cv2.imshow("my_video",frame)
+
+    key = cv2.waitKey(1)
+
+
+    if key == ord("q"):
+        break
+video.release()
+
+
