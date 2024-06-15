4dde842c69ba84675812b5acd61c8908640866b3
jagadeesh-sagar <nagarajug176@gmail.com>
2024-06-15 14:29:52 +0530

    Extract the images from the webcam

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import cv2
 import time
+import glob
+from send import send_email
 
 video=cv2.VideoCapture(0)
 time.sleep(2)
 first_frame=None
+status_list = []
+count=1
+
 while True:
+    status=0
     check, frame=video.read()
+
     gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray_frame_gau=cv2.GaussianBlur(gray_frame,(21,21),0)
 
@@ -23,13 +30,29 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y),(x+w,y+h),(0,255,0),3)
+        rectangle=cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        if rectangle.any():
+            status=1
+            cv2.imwrite(f"images/{count}.png", rectangle)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object=all_images[index]
+            print(image_with_object)
+
+
+    status_list.append(status)
+    status_list=status_list[-2:]
+    if status_list[0]==1 and status_list[1]==0:
+
+        send_email()
+
 
     cv2.imshow("My video",frame)
     key = cv2.waitKey(1)
 
 
-    if key == ord("q"or"Q"):
+    if key == ord("q"):
         break
 video.release()
 
